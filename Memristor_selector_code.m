e36a89babf620ccfbe6be221dfee79fe049c495d
Yasir Noori <56324216+yjnoori@users.noreply.github.com>
2019-10-24 11:50:11 +0100

    Update Memristor_selector_code.m

--- Memristor_selector_code.m
+++ Memristor_selector_code.m
@@ -1,6 +1,11 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-      %**Welcome to Noori's memristor array simulator**%      
-             %**Selector + Memristor arrays**%
+%**Welcome to Noori's memristor array simulator**%      
+%**Selector + Memristor arrays**%
+%If you found this code useful for your work, the authors would appreciate
+%your citation of the paper: Y J Noori, C H de Groot "Modelling Resistive
+%and Phase Change Memory with Passive Selector Arrays- A Matlab Tool"
+%arXiv:1910.05836 (2019). Doing so, will ensure that the authors continue
+%on updating the code and keep it open-access.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
